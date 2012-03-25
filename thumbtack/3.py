1d8060ab4d80484e126efef36459f425f601272d
Jameson Lee <jamesonjlee@gmail.com>
2012-03-24 17:40:00 -0700

    [thumbtack] representation done

--- thumbtack/3.py
+++ thumbtack/3.py
@@ -11,14 +11,13 @@
 
 from collections import deque
 import sys
+import readline
 
 
 MAX_DAYS = 20
 
 servicers = []
 requests = deque()
-# servicers.sort(key = len)
-servicers.sort(lambda a,b: cmp(len(a), len(b)))
 
 def _reset():
   global servicers, requests, MAX_DAYS
@@ -28,9 +27,27 @@ def _reset():
 
 def _match():
   global servicers, requests
+  servicers.sort(key = len)
+  #servicers.sort(lambda a,b: cmp(len(a), len(b)))
+  print servicers
+  print requests
   return 0
 
 def _process(servicers, requests, query):
+  _type = query[0].lower()
+  #_name = query[1]
+  if (_type == "service"):
+    servicers.append(query[1:])
+  elif (_type == "request"):
+    _event, _job, _date = query[1:]
+    if _date.isdigit():
+      requests[int(_date)-1].append([_job, _event])
+    else:
+      _d = _date.split('-')
+      for _date in xrange(int(_d[0]),int(_d[1])+1):
+        requests[int(_date)-1].append([_job, _event])
+  else:
+    print "line is not [service] or [request], skipping"
   return True
 
 if __name__ == "__main__":
@@ -39,12 +56,17 @@ if __name__ == "__main__":
   if len(sys.argv) > 1:
     lines = open(sys.argv[1])
   else:
-    lines = sys.stdin
-  for line in lines:
+    lines = False
+
+  while (1):
+    line = lines.readline() if lines else sys.stdin.readline()
+    #line = line.lower().split() #caps shouldn't matter...
+    line = line.split()
     if len(line) is 0:
+      if (lline == line): exit(0)
       print _match()
       _reset()
     else:
-      this is old instand
-      # old instance
+      _process(servicers, requests, line)
+    lline = line
 
